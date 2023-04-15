bd318dc4d3333f2c22aa306aaa42c96517ad9a39
Tegridy <jarjak4@gmail.com>
2023-04-15 16:28:00 +0200

    Keycloak api url fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_URL: "https://tegirdyapi.pl/ho",
-  KEYCLOAK_URL: "https://tegirdyapi.pl",
+  KEYCLOAK_URL: "https://tegirdyapi.pl/",
   REDIRECT_URL: "https://gentle-froyo-5a1b8e.netlify.app/"
 };
