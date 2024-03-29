fe0b709d69bad8a04cee56d90f2de2ebd19cbec1
Tegridy <jankes1122@gmail.com>
2024-03-29 13:42:06 +0100

    API url update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: "https://tegridyapi.ovh/ho",
-  KEYCLOAK_URL: "https://tegridyapi.ovh/",
+  API_URL: "https://teapot.tailab9ad.ts.net/ho",
+  KEYCLOAK_URL: "https://teapot.tailab9ad.ts.net/",
   REDIRECT_URL: "https://gentle-froyo-5a1b8e.netlify.app/"
 };
